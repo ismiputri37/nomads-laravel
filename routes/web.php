e4ce10b520f99bfc8b84760ccd86fc9edb2e3f76
ismiputri37 <ismi.putri397@gmail.com>
2025-03-19 05:12:06 +0800

    admin layout and templating blade done

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+
+
+Route::get('/', [HomeController::class, 'index'] )->name('home');
+Route::get('/detail', [DetailController::class, 'index'] )->name('detail');
+Route::get('/checkout', [CheckoutController::class, 'index'] )->name('checkout');
+Route::get('/checkout/success', [CheckoutController::class, 'success'] )->name('checkout-success');
+
+
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
